a1b69f1d404413d84da207dfa07764e72ca3900a
ijaguirre <ijaguirre@uc.cl>
2017-06-27 00:52:18 -0400

    initial commit

---
+++ js/base-map.js
@@ -0,0 +1,14 @@
+// Set view of Leaflet map based on screen size
+var layer = new L.tileLayer('http://{s}.tile.thunderforest.com/outdoors/{z}/{x}/{y}.png?apikey={apikey}', {
+	attribution: '<a href= "mailto:ijaguirre@uc.cl" target="_blank"> Cartografia: Ignacio Aguirre &copy <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+	apikey: 'efbb614508e045beb3c1002201c66ba2',
+	maxZoom: 22
+});
+
+if ($(window).width() < 626) {
+	var map = new L.Map('map').setView([-35.406654326, -71.664995696],21);
+
+} else {
+	var map = new L.Map('map').setView([-35.406654326,	-71.664995696],21);
+}
+map.addLayer(layer);
